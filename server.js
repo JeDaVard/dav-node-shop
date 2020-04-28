ac51bf28bad8e142fa3b5a34234f743e23d63955
Davit Vardanyan <jedavard@gmail.com>
2020-04-28 20:11:30 +0400

    sensitive data config

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const fs = require('fs');
 const port = process.env.PORT;
 
 // SSL
-const privateKey = fs.readFileSync('-server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('-server.key');
+// const certificate = fs.readFileSync('server.cert');
+// const server =
+//     process.env.NODE_ENV === 'production'
+//         ? https.createServer({ key: privateKey, cert: certificate }, app)
+//         : app;
 
-const server =
-    process.env.NODE_ENV === 'production'
-        ? https.createServer({ key: privateKey, cert: certificate }, app)
-        : app;
+const server = app;
 
 server.listen(port, () => {
     console.log(`Server is up on ${port}`);
